2a74fada9715df593d1b0b9982a0106ecd77deb8
Mohamed amaan <mohamedamaan319@gmail.com>
2026-05-26 11:42:23 +0530

    readme updated

--- components/services-section.tsx
+++ components/services-section.tsx
@@ -77,9 +77,11 @@ export function ServicesSection() {
               <p className="text-[18px] leading-[30px] font-medium text-[#393939] mb-8">
                 Looking for another service? Get in touch with me, there is a high chance that I will be able to help!
               </p>
-              <Button className="bg-black text-white hover:bg-black/90 rounded-[16px] px-12 py-6 font-medium text-[18px] w-full max-w-[340px] h-[64px]">
-                <Mail className="w-5 h-5 mr-2" />
-                Get in touch
+              <Button asChild className="bg-black text-white hover:bg-black/90 rounded-[16px] px-12 py-6 font-medium text-[18px] w-full max-w-[340px] h-[64px]">
+                <a href="mailto:mohamedamaan319@gmail.com">
+                  <Mail className="w-5 h-5 mr-2" />
+                  Get in touch
+                </a>
               </Button>
             </div>
           </div>
